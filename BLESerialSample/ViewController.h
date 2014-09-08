c5b9572459e033eb88792c99976507a5b4d4029a
Kawashima Daichi <daichi0.0daichi@gmail.com>
2014-09-08 22:41:48 +0900

    Initial Commit

---
+++ BLESerialSample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BLESerialSample
+//
+//  Created by 川島 大地 on 2014/09/08.
+//  Copyright (c) 2014年 川島 大地. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ BLESerialSample/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  BLESerialSample
+//
+//  Created by 川島 大地 on 2014/09/08.
+//  Copyright (c) 2014年 川島 大地. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
